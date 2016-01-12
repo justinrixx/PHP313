8d6bc052fa40f0c1de4a9456e352c1121b6e178e
Justin Ricks <justin.rixx@gmail.com>
2016-01-12 15:00:41 -0700

    Add an image to the main page

--- js/init.js
+++ js/init.js
@@ -3,5 +3,7 @@
 
     $('.button-collapse').sideNav();
 
+    $('#index-banner').css("backgroundImage", "url('img/wedding.jpg')").css("backgroundSize", "cover").css("backgroundPosition", "0px -50px");
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
