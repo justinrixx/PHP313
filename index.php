851f97f45fcb504c4e2b71a5864d9d9fc23aeec9
Justin Ricks <justin.rixx@gmail.com>
2016-01-12 13:53:06 -0700

    Change from the basic hello world to a starting place

--- index.php
+++ index.php
@@ -1,5 +1,116 @@
-<?php
+<!DOCTYPE html>
 
-echo "Hello world ";
+<html lang="en">
 
-?>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>Justin Ricks</title>
+
+  <!-- CSS
+       I'm using CSS from the Materialize project in order to make my site conform to
+       material design -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+
+<body>
+  <nav class="light-blue lighten-1" role="navigation">
+  <!--
+    Empty, just because I want the blue bar across the top
+    -->
+  </nav>
+
+  <div class="section no-pad-bot" id="index-banner">
+    <div class="container">
+      <br><br>
+      <h1 class="header center orange-text">Justin Ricks</h1>
+      <div class="row center">
+        <h5 class="header col s12 light">A little about myself</h5>
+      </div>
+      <div class="row center">
+        <a href="assignments.html" id="download-button" class="btn-large waves-effect waves-light orange">Assignments</a>
+      </div>
+      <br><br>
+    </div>
+  </div>
+
+
+  <div class="container">
+    <div class="section">
+
+      <!--   Icon Section   -->
+      <div class="row">
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center light-blue-text"><i class="material-icons">code</i></h2>
+            <h5 class="center">Computer Science major</h5>
+
+            <p class="light">I'm a student studying computer science at BYU-Idaho. I'm actually from Rexburg,
+            and enjoy it here. I really enjoy tackling hard problems, and the more I learn about computer
+            science, the more I want to learn.</p>
+          </div>
+        </div>
+
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center light-blue-text"><i class="material-icons">group</i></h2>
+            <h5 class="center">User Experience Focused</h5>
+
+            <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+          </div>
+        </div>
+
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center light-blue-text"><i class="material-icons">settings</i></h2>
+            <h5 class="center">Easy to work with</h5>
+
+            <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <br><br>
+
+    <div class="section">
+
+    </div>
+  </div>
+
+  <footer class="page-footer orange">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">Android Apps</h5>
+          <p class="grey-text text-lighten-4">If you're interested in looking at some of the Android apps
+          I've built myself and with others, check these out.</p>
+
+
+        </div>
+        <div class="col l3 s12">
+          <ul>
+            <li><a class="white-text" href="https://play.google.com/store/apps/details?id=com.gmail.rixx.justin.envelopebudget">Cash Caddy</a></li>
+            <li><a class="white-text" href="https://play.google.com/store/apps/details?id=com.gmail.app.studios.on.game">Game On</a></li>
+            <li><a class="white-text" href="https://play.google.com/store/apps/details?id=com.gmail.rixx.justin.sieveoferatosthenes">Sieve of Eratosthenes</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      
+      </div>
+    </div>
+  </footer>
+
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+  </body>
+</html>
