e36641cc7b348bd12322a82fd9bde316c626c53f
Justin Ricks <justin.rixx@gmail.com>
2016-01-12 14:20:20 -0700

    Add the assignments page

--- index.php
+++ index.php
@@ -10,8 +10,12 @@
   <!-- CSS
        I'm using CSS from the Materialize project in order to make my site conform to
        material design -->
+
+  <!-- This one is for the material design icons -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!-- For the main materialize stuff -->
   <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <!-- For my own stuff -->
   <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
 
